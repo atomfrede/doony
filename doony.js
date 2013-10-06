abdd067eb755fe1e70239ccc2e2df3d8dfe1671a
Frederik Hahne <frederik.hahne@gmail.com>
2013-10-06 22:18:27 +0200

    Update doony.js

--- doony.js
+++ doony.js
@@ -576,6 +576,12 @@ jQuery(function($) {
                 $(this).replaceWith("<i class='icon-warning-sign icon-3x'> </i>");
             }
         }
+        
+         if(source != undefined && width == 24 && height == 24){
+             if(source.indexOf("clock.png") != -1){
+                 $(this).replaceWith("<i class='icon-play icon-large'> </i>");
+             }
+         }
         //console.log($(this).attr("src"));
     });
     
